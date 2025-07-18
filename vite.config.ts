5bb52fe6a27b930e19f094f344802d7c6f137a0f
athipan1 <166430089+athipan1@users.noreply.github.com>
2025-07-18 12:44:35 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+export default defineConfig({
+  base: '/PaiNaiDee_Frontend/', // ชื่อ repo
+  ...
+})
